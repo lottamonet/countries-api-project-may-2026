67770523b43620744d00fd3a86fcd61b9ef94ebc
LottaMonet <lotta@anniecannons.com>
2026-07-22 13:19:44 -0700

    update gitignore
    stop tracking config.js

--- version-3/server/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    databaseUrl:
-        "postgresql://neondb_owner:npg_1YehnCiKL7yP@ep-royal-snow-afzu06pb-pooler.c-2.us-west-2.aws.neon.tech/countries-api-db?sslmode=require&channel_binding=require",
-};
-
-export default config;
